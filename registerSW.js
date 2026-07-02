930717877184080862a4419a155ec56386fd95df
yangbrulapha-tech <yangbrulapha@gmail.com>
2026-07-02 06:12:25 +0000

    Deploying to gh-pages from @ yangbrulapha-tech/react-ecommerce-template@c4f6b6c1548509a743cb3b05ce94c8b84cafd9a4 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/react-ecommerce-template/sw.js', { scope: '/react-ecommerce-template/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
